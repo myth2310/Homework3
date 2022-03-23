5c9313fe4daf05bc425e2b8d5ec293dc46bc5490
Achmad Miftahudin <93859870+myth2310@users.noreply.github.com>
2022-03-23 12:21:22 +0700

    Update App.js
    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './pages/home/indexx';
+import Data from "./data.json";
 
+  
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+    return (
+        <div className="App">
+            <header className="App-header">
+            {/* <Home />
+          <div className='search'>
+              <input placeholder='Search...'></input>
+              <button>Search</button>
+        </div> */}
+         
+
+
+         {Data.map(data => (
+        <div className="playlist-container">
+          <div className="playlist-item">
+            <img
+              className="playlist-image"
+              src="https://pophariini.com/wp-content/uploads/2020/11/Art-work-Senja-Diaskara-Bintang-1068x1068.jpg"
+              alt="playlist"
+            />
+            
+            <div className="playlist-content">
+              <h2 className="playlist-title">{data.album}</h2>
+              <h3 className="playlist-description">{data.name} ~ {data.album}</h3>
+            </div>
+          
+            <div className="playlist-actions">
+              <button className="playlist-action" id=''>Select</button>
+            </div>
+          </div>
+        </div>
+        ))}
+         </header>
+        </div>
+    );
 }
 
 export default App;
