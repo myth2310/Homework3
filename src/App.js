395b907d6c44ee15541560e49172549f91a2304a
Achmad Miftahudin <93859870+myth2310@users.noreply.github.com>
2022-03-25 21:29:49 +0700

    Update App.js
    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,33 +9,51 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-            {/* <Home />
-          <div className='search'>
-              <input placeholder='Search...'></input>
-              <button>Search</button>
-        </div> */}
+//             {/* <Home />
+//           <div className='search'>
+//               <input placeholder='Search...'></input>
+//               <button>Search</button>
+//         </div> */}
          
 
 
-        <div className="playlist-container">
-          <div className="playlist-item">
-            <img
-              className="playlist-image"
-              src={Data.album.imgAlbum}
-              alt="playlist"
-            />
-            <div className="playlist-content">
-              <h2 className="playlist-title">{Data.album.name}</h2>
-              <div className='desc'>
-              <p>{Data.album.Art}</p>
-              <p>{Data.album.total_tracks} Songs</p>
-              </div>
-            </div>
-            <div className="playlist-actions">
-              <button className="playlist-action" id=''><a href={Data.external_urls.spotify}>Select</a></button>
-            </div>
-          </div>
-        </div>
+//         <div className="playlist-container">
+//           <div className="playlist-item">
+//             <img
+//               className="playlist-image"
+//               src={Data.album.imgAlbum}
+//               alt="playlist"
+//             />
+//             <div className="playlist-content">
+//               <h2 className="playlist-title">{Data.album.name}</h2>
+//               <div className='desc'>
+//               <p>{Data.album.Art}</p>
+//               <p>{Data.album.total_tracks} Songs</p>
+//               </div>
+//             </div>
+//             <div className="playlist-actions">
+//               <button className="playlist-action" id=''><a href={Data.external_urls.spotify}>Select</a></button>
+//             </div>
+//           </div>
+//         </div>
+
+               <table>
+                <tr>
+                  <th>Album</th>
+                  <th>Title</th>
+                  <th>Total Track</th>
+                  <th>Total Track</th>
+                </tr>
+                {Data.map((data) => (  
+               <tr>   
+                 <td>{data.album.name}</td>
+                 <td>{data.name}</td> 
+                 <td>{data.album.total_tracks}</td>  
+                 <td className='Link'><a>{data.external_urls.spotify}</a></td> 
+               </tr>  
+             ))}  
+                
+              </table>
        
          </header>
         </div>
